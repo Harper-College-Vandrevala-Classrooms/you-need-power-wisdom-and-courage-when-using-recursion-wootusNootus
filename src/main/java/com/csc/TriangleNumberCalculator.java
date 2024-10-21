c23e4ce64b469de99ada1e5e55fb2b4b68b8bf7b
wootusNootus <migospodinoff@gmail.com>
2024-10-20 22:06:36 -0500

    Tests working yay

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -1,5 +1,22 @@
 package com.csc;
 
 public class TriangleNumberCalculator {
-  // Your code goes here!
+  // Returns nth triangular number
+  public int value(int n){
+    if(n == 0){
+      return 0; // last case where n = 0
+    }
+
+    return n + value(n - 1);
+  }
+
+  // Adds nth and mth triangular numbers
+  public int add(int n, int m){
+    return value(n) + value(m);
+  }
+
+  // Subtracts nth and mth triangular numbers
+  public int subtract(int n, int m){
+    return value(n) - value(m);
+  }
 }
--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,6 +1,9 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
 
@@ -11,5 +14,32 @@ public class TestTriangleNumberCalculator {
     calculator = new TriangleNumberCalculator();
   }
 
-  // Your tests go here
+  // Testing known triangular numbers
+  @Test
+  public void testValue() {
+    assertEquals(0, calculator.value(0));
+    assertEquals(1, calculator.value(1));
+    assertEquals(3, calculator.value(2));
+    assertEquals(6, calculator.value(3));
+    assertEquals(10, calculator.value(4));
+    assertEquals(15, calculator.value(5));
+  }
+
+  // Test for the add function 
+  @Test
+  public void testAdd() {
+      assertEquals(1, calculator.add(0, 1));
+      assertEquals(4, calculator.add(1, 2));
+      assertEquals(25, calculator.add(4, 5));
+      assertEquals(9, calculator.add(3, 2));
+  }
+
+  // Test for the subtract function
+  @Test
+  public void testSubtract() {
+      assertEquals(-2, calculator.subtract(1, 2));
+      assertEquals(9, calculator.subtract(5, 3));
+      assertEquals(55, calculator.subtract(10, 0));
+      assertEquals(0, calculator.subtract(3, 3));
+  }
 }
